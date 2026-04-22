a6f2cc1c5447c2e65947cd9b74e06c2fb46d79ff
ludihan <65617704+ludihan@users.noreply.github.com>
2026-04-22 12:32:58 -0400

    fix: vendorHash in nix flake

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
           todo = pkgs.buildGoModule {
             pname = "todo";
             version = "0.0.1";
-            vendorHash = "sha256-8uKnWsQZEzLV5t8wtYqAONrsIRWdZx7OF7DAeOMckTU=";
+            vendorHash = "sha256-GkW+W3Mwv9RqV9XcxKR8zD4q3p/w9mmTRrRuYRR7E9M=";
             src = ./.;
           };
         }
